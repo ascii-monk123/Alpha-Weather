03b3a7e1eadafa287212eeed7006340728fac1e9
aahan_5833 <aahancharak@gmail.com>
2019-11-27 05:11:00 +0530

    Completed the heading section of the web app

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -2,14 +2,11 @@ import 'bootstrap';
 import '../scss/app.scss';
 import x from './test';
 var $ = require('jquery');
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 console.log(`Imported ${x} from another module and made some changes asuzi`);
 
-$(document).ready(function() {
-  $('.pidgey').fadeOut(2000);
-});
-
-document.querySelector('.poom').addEventListener('click', e => {
-  setTimeout(() => {
-    e.target.style.display = 'none';
-  }, 2000);
-});
+$('.hero__img__box').fadeOut(0);
+$('.hero__img__box').fadeIn(2000);
