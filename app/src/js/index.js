be4d8e6eb86b3b8f27e431b5d2252436062e9f1b
aahan_5833 <aahancharak@gmail.com>
2019-11-28 06:27:35 +0530

    Added the axios configs and some api key configs

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,12 +1,31 @@
 import 'bootstrap';
 import '../scss/app.scss';
-import x from './test';
+import axios from 'axios';
+
 var $ = require('jquery');
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
-console.log(`Imported ${x} from another module and made some changes asuzi`);
 
 $('.hero__img__box').fadeOut(0);
 $('.hero__img__box').fadeIn(2000);
+
+/*
+//How to fetch the data from the open weather source map api
+const checkData = async query => {
+  try {
+    const data = await axios(
+      'https:\\api.openweathermap.org/data/2.5/weather?q=London&&APPID=600409249d829db023a38425bd27549b'
+    );
+    console.log(data.data.main.temp_max);
+  } catch (err) {
+    console.log(error);
+  }
+};
+document
+  .querySelector('.query__fetch__btn')
+  .addEventListener('click', checkData);
+
+
+  */
